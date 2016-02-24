2a288537b2be0ff5783f9ff27b20e3f2164ebb7c
cjempey <qspamcan@gmail.com>
2016-02-23 19:41:27 -0800

    Commit first classes
    interface Solution and implementor SimpleSolutionImpl, along with test
    cases for SimpleSolutionImpl

---
+++ groovy-optimizer/src/test/groovy/name/cjempey/optimizer/SimpleSolutionImplTests.groovy
@@ -0,0 +1,27 @@
+package name.cjempey.optimizer;
+
+import name.cjempey.optimizer.impl.SimpleSolutionImpl;
+import spock.lang.*
+
+class SimplSolutionImplTests extends Specification {
+	SimpleSolutionImpl cut = new SimpleSolutionImpl(5);
+	
+	def "fitness gives stored number"() {
+		expect:
+			cut.fitness == 5;
+	}
+	
+	def "crossover gives average"() {
+		setup:
+			SimpleSolutionImpl other = new SimpleSolutionImpl(7);
+		expect:
+			cut.cross(other).fitness == 6;
+	}
+	
+	def "mutate gives +1 or -1"() {
+		when:
+			cut.mutate();
+		then:
+			cut.fitness == 6 || cut.fitness == 4
+	}	
+}
